9ecea6de4b016b5e182442cd7fef78f09441a4b2
EC2 Default User <ec2-user@ip-172-31-21-30.us-east-2.compute.internal>
2018-02-25 04:22:58 +0000

    お気に入り機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.add_favorite(micropost)
     flash[:success] = 'お気に入りに追加しました'
-    redirect_to user_path(current_user.id)
+    redirect_to favorites_user_path(current_user)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.remove_favorite(micropost)
     flash[:danger] = 'お気に入りから外しました'
-    redirect_to user_path(current_user.id)
+    redirect_to favorites_user_path(current_user)
   end
 end
